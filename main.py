5984675aabfbd350a64d3e72cfa8391caf4a2ed8
cristal-node <12prottoy2002@gmail.com>
2021-05-20 01:39:38 +0900

    print mistake...

--- main.py
+++ main.py
@@ -16,14 +16,10 @@ pool = []
 
 class mikudb():
 	def __init__(self):
-		# for i in range(self.page_number()):
-		# 	i +=1
-		# 	print(f"Working on page {i}")
-		# 	self.page(i);
-		print("pool starting!")
 		pages_results = ThreadPool(30).imap_unordered(self.page, range(1,self.page_number()+1))
 		for r in pages_results:
 			print(r)
+		print("pool starting!")
 		results = ThreadPool(30).imap_unordered(self.article, pool)
 		for r in results:
 			print(r)
